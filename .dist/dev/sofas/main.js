a4dc77796f5170072feed4db0efe74aa4cf5a8dc
Mike Laurel <mlaurel@hey.com>
2024-04-18 12:59:33 -0700

    Initial commit

---
+++ .dist/dev/sofas/main.js
@@ -0,0 +1,18 @@
+// sofas/main.js
+class ColorSelector extends HTMLDivElement {
+  constructor() {
+    super();
+    const els = [...this.children];
+    els.forEach((el, i) => {
+      el.onclick = () => {
+        els.forEach((el2) => el2.removeAttribute("selected"));
+        const shade = el.getAttribute("shade");
+        preview.setAttribute("shade", shade);
+        el.setAttribute("selected", 1);
+      };
+      if (!i)
+        el.setAttribute("selected", 1);
+    });
+  }
+}
+customElements.define("color-selector", ColorSelector, { extends: "div" });
